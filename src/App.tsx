4d40f37d937c2c13e05f9be7b7975eb1700f0913
Rakshit <rakshit087@gmail.com>
2024-08-23 23:56:23 +0530

    chore: added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import "./App.css";
-
 function App() {
   return (
-    <>
-      <h1>पुरानी Kitabe</h1>
-      <div className="card">No Books found!</div>
-    </>
+    <div className="min-w-72 flex flex-col justify-center items-center py-4">
+      <h1 className="text-3xl font-bold underline">पुरानी Kitabe</h1>
+      <div className="mt-4">No Books found!</div>
+    </div>
   );
 }
 

ce3108c8237b4da6ad4064fcffb8463bc01fec61
Rakshit <rakshit087@gmail.com>
2024-08-23 11:20:30 +0530

    chore: installed vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>पुरानी Kitabe</h1>
+      <div className="card">No Books found!</div>
+    </>
+  );
+}
+
+export default App;
